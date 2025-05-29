4f604006c4ebfc506e54d47836f1153d31635681
Carlo de Falco <carlo.defalco@polimi.it>
2025-05-29 13:48:44 +0200

    simpler computation of stiffness matrix when no advection applied

--- inst/bim2a_laplacian.m
+++ inst/bim2a_laplacian.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2006,2007,2008,2009,2010  Carlo de Falco, Massimiliano Culpo
+## Copyright (C) 2006,2007,2008,2009,2010,2025  Carlo de Falco, Massimiliano Culpo
 ##
 ## This file is part of:
 ##     BIM - Diffusion Advection Reaction PDE Solver
@@ -16,8 +16,8 @@
 ##  You should have received a copy of the GNU General Public License
 ##  along with BIM; If not, see <http://www.gnu.org/licenses/>.
 ##
-##  author: Carlo de Falco     <cdf _AT_ users.sourceforge.net>
-##  author: Massimiliano Culpo <culpo _AT_ users.sourceforge.net>
+##  author: Carlo de Falco     
+##  author: Massimiliano Culpo 
 
 ## -*- texinfo -*-
 ##
@@ -37,17 +37,80 @@
 ## @seealso{bim2a_rhs, bim2a_reaction, bim2a_advection_diffusion, bim1a_laplacian, bim3a_laplacian}
 ## @end deftypefn
 
-function [A] = bim2a_laplacian(mesh,epsilon,kappa)
+function A = bim2a_laplacian (mesh, epsilon, kappa)
 
   ## Check input
   if nargin != 3
     error("bim2a_laplacian: wrong number of input parameters.");
+  elseif !(isstruct(mesh)     && isfield(mesh,"p") &&
+	   isfield (mesh,"t") && isfield(mesh,"e"))
+    error("bim2a_laplacian: first input is not a valid mesh structure.");
   endif
 
-  ## Input check inside bim2a_advection_diffusion
-  nnodes = columns(mesh.p);
-  nelem  = columns(mesh.t);
+  p      = mesh.p;
+  t      = mesh.t;
+  nnodes = columns(p);
+  nelem  = columns(t);
+
+  ## Turn scalar input to a vector of appropriate size
+  if isscalar(epsilon)
+    epsilon  = epsilon * ones (nelem, 1);
+  endif
+  if isscalar(kappa)
+    kappa = kappa * ones (nnodes, 1);
+  endif
+
+  if !( isvector (epsilon) && isvector (kappa) && (numel (epsilon) == nelem) && (numel (kappa) == nnodes))
+    error("bim2a_laplacian: coefficients are vectors of correct size.");
+  endif
+
+  ## Local element matrices (one 3x3 matrix for each triangle in the mesh)
+  Lloc = zeros(3, 3, nelem);
+
+  ## To integrate constants over each triangle we need to multiply by
+  ## the triangle area. Multiply by the diffusion coefficient now tu
+  ## simplify subsequent computations. Use inverse average for the
+  ## diffusion coefficient.
+  kappaepsilonareak = reshape (3./sum (1./kappa(:)(mesh.t (1:3)), 1)(:) .* epsilon(:) .* mesh.area(:), 1, 1, nelem);
+  shg = mesh.shg(:,:,:);
   
-  A = bim2a_advection_diffusion (mesh,epsilon,kappa,ones(nnodes,1),0);
+  ## Computation
+  for inode = 1:3
+    for jnode = 1:3
+      ginode(inode,jnode,:) = mesh.t(inode,:);
+      gjnode(inode,jnode,:) = mesh.t(jnode,:);
+      Lloc(inode,jnode,:)   = sum (shg(:,inode,:) .* shg(:,jnode,:), 1) .* kappaepsilonareak;
+    endfor
+  endfor
+
+  ## Assemble the local (full) matrices into one global (sparse) matrix
+  A = sparse (ginode(:), gjnode(:), Lloc(:));
 
 endfunction
+
+%!test
+%! m = msh2m_structured_mesh (0:.1:5, 0:.1:1, 1, 1:4, 'random');
+%! m = bim2c_mesh_properties (m);
+%! A = bim2a_laplacian (m, 1, 3);
+%! dnodes = bim2c_unknowns_on_side (m, 1:4);
+%! inodes = setdiff (1:columns(m.p), dnodes);
+%! u = m.p(1,:)';
+%! u(inodes) = A(inodes, inodes) \ (-A(inodes, dnodes) * u(dnodes));
+%! assert (u, m.p(1,:)', sqrt(eps))
+
+
+%!demo
+%! m = msh2m_structured_mesh (0:.1:2*pi, 0:.1:2*pi, 1, 1:4, 'random');
+%! m = bim2c_mesh_properties (m);
+%! kappa = 2 + sin (m.p(1, :)');
+%! f     = kappa .* cos (m.p(2, :)');
+%! uex   = cos (m.p(2, :)');
+%! A = bim2a_laplacian (m, 3./(sum(1./kappa(m.t(1:3, :)), 1)), 1);
+%! b = bim2a_rhs (m, 1, f);
+%! dnodes = bim2c_unknowns_on_side (m, 1:4);
+%! inodes = setdiff (1:columns(m.p), dnodes);
+%! u = uex;
+%! u(inodes) = A(inodes, inodes) \ (b(inodes)-A(inodes, dnodes) * u(dnodes));
+%! h = pdesurf (m.p, m.t, u)
+%! figure
+%! pdesurf (m.p, m.t, uex)
